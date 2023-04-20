b3bd374b5223d8e6d8f60c7ab10c71d5126e65f5
Đặng Hoàng Phúc <hoangphucmagic@gmail.com>
2023-04-20 21:50:50 +0700

    feat: htmtl css basic

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const menu= document.querySelector(".navbar__links")
+const menuButton = document.querySelector(".navbar__icons")
+menuButton.addEventListener('click', ()=>{
+    menu.classList.toggle(".navbar__open")
+})
